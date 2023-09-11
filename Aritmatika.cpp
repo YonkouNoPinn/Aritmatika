312df7ce66322568f873688dd0186a08ed2c6962
Kelvinn XTkj2 <144640412+YonkouNoPinn@users.noreply.github.com>
2023-09-11 09:22:35 +0700

    Create Aritmatika.cpp

---
+++ Aritmatika.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a,b;
+cout<<"PEMROGAMAN OPERASI ARITMATIKA \n";
+cout<<"masukkan angka pertama: ";
+cin>>a;
+cout<<"masukkan angka kedua: ";
+cin>>b;
+cout<<"hasilnya penjumlahan: "<< a+b <<endl;
+
+cout<<"hasilnya pengurangan: "<< a-b <<endl;
+
+cout<<"hasilnya perkalian: "<< a*b <<endl;
+
+cout<<"hasilnya pembagian: "<< a/b <<endl;
+cout<<"hasil modulus:"<<a%b <<endl;
+return 0;
+}
